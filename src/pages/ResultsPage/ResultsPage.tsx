3ac618692838f37f4b0c29b2480ed662a260f3f3
Hayden Taylor <h.taylor2106@gmail.com>
2025-04-08 14:55:29 -0400

    add career card design

--- src/pages/ResultsPage/ResultsPage.tsx
+++ src/pages/ResultsPage/ResultsPage.tsx
@@ -10,7 +10,32 @@ export function ResultsPage({setPage}: ResultsPageProps) {
     const [index, setIndex] = useState<number>(0);
     return (
         <div className="main-container">
-            
+            <div className="careerCards">
+                <div className="career">
+                    <div className="jobTitle">
+                        Job Title
+                    </div>
+                    <div className="jobDesc">
+                        This is a brief description for the given career.
+                    </div>
+                    <hr/>
+                    <div className="jobReason">
+                        Here is my opinion on why I think this is a good fit for you.
+                    </div>
+                </div>
+                <div className="career">
+                    <div className="jobTitle">
+                        Job Title
+                    </div>
+                    <div className="jobDesc">
+                        This is a brief description for the given career.
+                    </div>
+                    <hr/>
+                    <div className="jobReason">
+                        Here is my opinion on why I think this is a good fit for you.
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
