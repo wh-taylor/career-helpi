6a796100ee98eaac8ded72fe807b70178a417e03
Hayden Taylor <h.taylor2106@gmail.com>
2025-04-28 01:10:52 -0400

    setup state in slider

--- src/pages/BasicQuiz/Components/Slider.tsx
+++ src/pages/BasicQuiz/Components/Slider.tsx
@@ -8,19 +8,26 @@ interface SliderProps {
 }
 
 export function Slider({index, options}: SliderProps) {
+    const [rating, setRating] = useState<number>(3);
+
+    function changeRating(event: React.ChangeEvent<HTMLInputElement>) {
+        setRating(parseInt(event.target.value));
+    };
+
     return(
-    <div className="answers">
-        <Form.Range 
-            className="slider"
-            min={1}
-            max={5}
-            step={1}
-        />
-        <div className="numberOptions">
-            {options.map((x) => <p>{x}</p>)}
+        <div className="answers">
+            <Form.Range 
+                className="slider"
+                min={1}
+                max={5}
+                step={1}
+                value={rating}
+                onChange={changeRating}
+            />
+            <div className="numberOptions">
+                {options.map((x) => <p>{x}</p>)}
+            </div>
         </div>
-        
-    </div>
     )
 }
 
